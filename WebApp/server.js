10b93824f0eca86af07c009833968cd32486a637
Brian Nippert <briannippert@gmail.com>
2021-10-10 21:26:12 -0400

    Added empty methods

--- WebApp/server.js
+++ WebApp/server.js
@@ -6,6 +6,27 @@ app.get('/', (req, res) => {
   res.send('API Running')
 })
 
+app.get("/getWeather", (req, res) => {
+  res.send('API Running')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+/**
+ * Function to get the latitude and longitude from a zip code.
+ * @param {Zip Code} zip 
+ */
+function getLocation(zip){
+
+}
+
+/**
+ * Function to get weather from NOAA using latitude and longitude
+ * @param {Latitude} lat 
+ * @param {Longitude} long 
+ */
+function getWeather(lat ,long ){
+
+}
