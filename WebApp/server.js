d0adab37c30051d88d1388470f5dd48dd298789f
Brian Nippert <briannippert@gmail.com>
2021-10-10 21:28:23 -0400

    Update server.js

--- WebApp/server.js
+++ WebApp/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+//use .env to replace this parameter.
 const port = 8080
 
 app.get('/', (req, res) => {
@@ -9,6 +10,7 @@ app.get('/', (req, res) => {
 app.get("/getWeather", (req, res) => {
   res.send('API Running')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
